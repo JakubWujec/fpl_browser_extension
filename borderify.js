e49e256ad5314f6a998072fd8d96f45e558ad643
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-08-16 11:57:13 +0200

    fetching data

--- borderify.js
+++ borderify.js
@@ -1,6 +1,8 @@
 
-let PLAYER_DIV_CLASS_NAME = "styles__PitchElementWrap-sc-hv19ot-0 LEzSi"
+let PLAYER_DIV_CLASS_NAME = "styles__PitchElementWrap-sc-hv19ot-0 LEzSi";
+let FPL_API_LINK = "https://fantasy.premierleague.com/api/bootstrap-static/";
 
+const PLAYERS_DATA = [];
 
 function makeBadge(text, color) {
     let badge = document.createElement("span")
@@ -17,33 +19,67 @@ function makeBadge(text, color) {
     return badge;
 }
 
-function makeBar() {
+function findTeamNameFromPlayerDiv(playerDiv) {
+    const imgElement = playerDiv.querySelector('img');
+    const altValue = imgElement.alt;
+    return altValue
+}
+
+function makeBar(children) {
     let row = document.createElement("div");
     row.style.display = "flex"
     row.style.flexWrap = "no wrap"
-    for (let i = 0; i < 5; i++) {
-        row.appendChild(
-            makeBadge("SWE", "red")
-        )
+    for (let child of children) {
+        row.appendChild(child)
     }
     return row;
 }
 
-function addBar(parentNode) {
-    parentNode.appendChild(
-        makeBar()
-    )
+async function fetchTeamData() {
+    const url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    const data = await fetchData(url)
+    return data["teams"];
 }
+
+async function fetchFixtures() {
+    const url = "https://fantasy.premierleague.com/api/fixtures/";
+    const data = await fetchData(url);
+    return data;
+}
+
+const fetchData = async (url) => {
+    console.log("FETCHING MY FIREND");
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+
+
 
 async function start() {
     await new Promise(r => setTimeout(r, 2000));
     let playerDivs = document.getElementsByClassName(PLAYER_DIV_CLASS_NAME);
+    console.log("BEFORE BEFORE")
+    let teamData = await fetchTeamData();
+    let fixtures = await fetchFixtures();
+    console.log("XXXXXXXXXXXXXXX")
     for (let playerDiv of playerDivs) {
-        playerDiv.style.border = "5px solid red";
-        addBar(playerDiv);
+        let teamName = findTeamNameFromPlayerDiv(playerDiv);
+        playerDiv.style.border = "2px solid red";
+
+
+        playerDiv.appendChild(
+            makeBar([
+                makeBadge(teamName, "blue"),
+                makeBadge(teamName, "blue"),
+                makeBadge(teamName, "blue"),
+            ])
+        )
     }
 }
 
 
 
 start();
+
